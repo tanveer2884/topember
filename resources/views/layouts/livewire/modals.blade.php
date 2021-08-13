546608075857b94793a587db530bdfccd16df14a
Muhammad Saqib Ali <saqib.topdot@gmail.com>
2021-08-13 16:32:47 +0500

    changed permission and some other things

--- resources/views/layouts/livewire/modals.blade.php
+++ resources/views/layouts/livewire/modals.blade.php
@@ -1,6 +1,5 @@
 @switch($modalType)
     @case('pre-alert-detail')
-        <x-modals.pre-alert-detail :id="$modalDataId"></x-modals.pre-alert-detail>
         @break
     @default
         Invalid Modal Type
