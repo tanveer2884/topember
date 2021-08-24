e2253f42322620330f9aaf638aa2b7e8e7237490
Muhammad Saqib Ali <saqib.topdot@gmail.com>
2021-08-24 18:05:24 +0500

    filter values remvoed from product view

--- resources/views/livewire/frontend/product/product-listing.blade.php
+++ resources/views/livewire/frontend/product/product-listing.blade.php
@@ -12,10 +12,10 @@
                     <p>
                         {!! $product->short_description !!}
                     </p>
-                    <div>
+                    {{-- <div>
                         {{ $product->attributes->pluck('name')->toJson() }}
                         {{ $product->attributeValues->pluck('name')->toJson() }}
-                    </div>
+                    </div> --}}
                     <div class="prdt-price d-flex">
                         <h4>${{ number_format($product->getPrice()) }}</h4>
                         {{-- <h4>${{ number_format($product->price) }}</h4> --}}
