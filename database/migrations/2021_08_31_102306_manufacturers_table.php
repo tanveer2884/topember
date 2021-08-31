b54c08ebef60568cde915360309715ba57a60393
kashif topdot <kashif@topdot.pk>
2021-08-31 18:11:40 +0500

    manufactures

--- database/migrations/2021_08_31_102306_manufacturers_table.php
+++ database/migrations/2021_08_31_102306_manufacturers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ManufacturerTable extends Migration
+class ManufacturersTable extends Migration
 {
     /**
      * Run the migrations.
