066c85a2fea1da69edad9afb6e40e2a810d56a79
Muhammad Saqib Ali <saqib.topdot@gmail.com>
2021-09-07 11:57:07 +0500

    Track a package page added

---
+++ resources/views/templates/Track-Package.blade.php
@@ -0,0 +1,56 @@
+<div class="track-main-wrapper" style="background-image:url('images/error-banner.png');">
+    <div class="track-pakg-wrap">
+        <h2>
+            Track a Package
+        </h2>
+        <div class="track-sub-pakg-wrap">
+            <h3>
+                Tracking
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, consec tetuer adipiscing elit, sed diam nibh euismod 
+                tincidunt ut laoreet dolore magna aliquam erat volutpat. ipsum dolor sit amet, 
+                consec tetuer adipiscing elit, 
+            </p>
+            [[Track-A-Package-Form-Placeholder]]
+        </div>
+    </div>
+
+    {{-- Result box --}}
+    <div class="track-pakg-wrap d-none">
+        <h2>
+            Track a Package
+        </h2>
+        <div class="track-sub-pakg-wrap">
+            <h3>
+                sample text here
+            </h3>
+            <h4>Tracking#:</h4>
+            <p>2AB45678</p><br>
+            <h4>Carrier:</h4>
+            <p>sample text here</p><br>
+            <h4>type of Package:</h4>
+            <p>sample text here</p><br>
+            <h4>estimated delivery date:</h4>
+            <p>December 30,2021</p><br>
+            <form>
+                <div class="form-group ch-box-1">
+                    <input type="checkbox" class="d-none" id="specification">
+                    <label for="specification" onclick="showInputTwo()">Email notification</label>
+                </div>
+                <div class="col-sm-12 d-none" id="nw-txt-area-Three">
+                    <div class="form-group">
+                        <input type="text" class="form-control forgot-sp-wide" id="exampleFormControlInput1" placeholder="Email">
+                        <div class="error d-none">error goes here!</div>
+                    </div>
+                </div>
+                
+                <div class="sign-in-side-butn">
+                    <a href="#" class="sign-in-butn general-btn">Submit</a>
+                    <!-- <a href="#" class="track-faqs text-uppercase">Tracking FAQ’s &#8594;</a> -->
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
