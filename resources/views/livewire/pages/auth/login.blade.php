def5ffe8f98afb9f4c7b549a7ab9f5403fd50c6e
muhammad awais <awais@topdot.pk>
2024-02-05 10:10:10 +0500

    remove forget admin password functionality

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -76,12 +76,5 @@ new #[Layout('layouts.guest')] class extends Component
                 <i class="text-xl leading-none las la-circle-notch la-spin"></i>
             </div>
         </button>
-
-        <p class="mt-4">
-            <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                href="{{ route('password.request') }}">
-                Forgot your password?
-            </a>
-        </p>
     </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 // Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+// Route::view('profile', 'profile')
+//     ->middleware(['auth'])
+//     ->name('profile');
 
 require __DIR__.'/auth.php';
