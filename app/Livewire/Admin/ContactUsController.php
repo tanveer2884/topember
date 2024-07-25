27f8a064b836ac22963ff2b84cae2e68b8c37587
Muhammad Tanveer <tanveer.topdot@gmail.com>
2024-07-25 18:35:24 +0500

    add contact us controller

---
+++ app/Livewire/Admin/ContactUsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class ContactUsController extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.contact-us-controller');
+    }
+}
